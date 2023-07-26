367139c5896287ef2a17af3c01b7d269a862fbfb
nova4212 <86191106+nova4212@users.noreply.github.com>
2023-07-26 17:17:39 +0900

    [Silver V] Title: K번째 수, Time: 1072 ms, Memory: 209052 KB -BaekjoonHub

---
+++ 백준/Silver/11004. K번째 수/K번째 수.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+#include <deque>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <math.h>
+
+using namespace std;
+typedef pair<int, int> node;
+int arr[50000001];
+int arr2[1000002];
+int idx[1000002];
+int idx2[1000002];
+
+int main()
+{
+    int cnt = 1;
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    int mod = 10;
+    int num=0;
+    int K;
+    int j = 0;
+    int biggest = 0;
+    int N;
+    cin >> N;
+    cin >> K;
+    for (int i = 0; i < N; i++)
+    {
+        cin >> arr[i];
+    }
+
+    sort(arr, arr + N);
+    cout << arr[K-1];
+    return 0;
+}
